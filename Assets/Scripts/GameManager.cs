593d5a6bac49424701fbb65f69e523e725c4010f
Laura Huovinen <laura.huovinen@outlook.com>
2020-10-23 12:31:12 +0300

    Basic menu and game manager
    - Add: Scene MainMenu
    - Add: Class GameManager
    - Add: Class MenuUIController
    - Player can press "play" from menu and get to the level straight away

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+
+namespace Bomberfox
+{
+    public static class GameManager
+    {
+        public enum Level
+        {
+            None = 0,
+            Menu,
+            InGame,
+            GameOver,
+            Shop,
+        }
+
+        public static readonly Dictionary<Level, string> LevelNames = new Dictionary<Level, string>()
+        {
+            {Level.Menu, "MainMenu"},
+            {Level.InGame, "TestingGrounds"}
+        };
+
+        public static bool ChangeLevel(Level level)
+        {
+            if (LevelNames.ContainsKey(level))
+            {
+                string levelName = LevelNames[level];
+                SceneManager.LoadScene(levelName);
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
