d70e372fee566ed8db89d2545c72d7d1df8cfabf
Eppu Syyrakki <eppu.syyrakki@tuni.fi>
2020-10-23 15:21:14 +0300

    Found top left square of tilemap

--- Assets/Scripts/LevelBuilder.cs
+++ Assets/Scripts/LevelBuilder.cs
@@ -1,18 +1,53 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class LevelBuilder : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	private readonly Vector3[] directions = { Vector3.up, Vector3.right, Vector3.down, Vector3.left };
+	private GameObject blocksParent = null;
+	private GameObject obstaclesParent = null;
+	private GameObject enemiesParent = null;
+	private int levelWidth, levelHeight;
+	private List<Vector2> reserved = new List<Vector2>();
+	private Vector3 topLeft;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	[SerializeField] private GameObject[] blockPrefabs;
+	[SerializeField] private GameObject[] obstaclePrefabs;
+	[SerializeField] private GameObject[] enemyPrefabs;
+
+	private void Awake()
+	{
+		FindParentObjects();
+		GetLevelSize();
+		topLeft = GetTopLeftVector();
+		print(topLeft);
+	}
+
+	private Vector3 GetTopLeftVector()
+	{
+		return new Vector3(-(levelWidth - 1) / 2, (levelHeight -1) / 2, 0);	
+	}
+
+	private void FindParentObjects()
+	{
+		blocksParent = GameObject.Find("Blocks");
+		obstaclesParent = GameObject.Find("Obstacles");
+		enemiesParent = GameObject.Find("Enemies");
+	}
+
+	private void GetLevelSize()
+	{
+		Tilemap tilemap = GameObject.Find("Tilemap").GetComponent<Tilemap>();
+		levelWidth = tilemap.size.x;
+		levelHeight = tilemap.size.y;
+	}
+
+	private void OnDrawGizmos()
+	{
+		Gizmos.color = Color.magenta;
+		Gizmos.DrawCube(topLeft, Vector3.one);
+	}
 }
