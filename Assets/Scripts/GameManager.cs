db7bfa4b71ee06d4bef6b12fd8926024b1f2cebe
Eppu Syyrakki <eppu.syyrakki@tuni.fi>
2020-10-25 22:07:06 +0200

    LevelBuilder tweaking
    Not-so-perfectly working

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,11 +8,13 @@ namespace Bomberfox
     {
         public static GameManager GM;
 
+        private bool keyCreated;
+
         void Awake()
         {
             if (GM != null)
             {
-                GameObject.Destroy(GM);
+                Destroy(GM);
             }
             else
             {
@@ -22,13 +24,14 @@ namespace Bomberfox
             DontDestroyOnLoad(this);
         }
 
-        void Start()
-        {
-            CreateKeyObstacle();
-        }
-
         void Update()
         {
+	        if (!keyCreated)
+	        {
+		        CreateKeyObstacle();
+		        keyCreated = true;
+	        }
+
             if (Input.GetKey(KeyCode.R))
             {
                 SceneManager.LoadScene((SceneManager.GetActiveScene().buildIndex));
@@ -45,7 +48,7 @@ namespace Bomberfox
             GameObject[] allObstacles = GameObject.FindGameObjectsWithTag("Obstacle");
             GameObject keyObstacle = allObstacles[Random.Range(0, allObstacles.Length)];
             keyObstacle.GetComponent<Obstacle>().IsKey = true;
-            Debug.Log("Key was hidden!");
+            Debug.Log("Key was hidden in " + keyObstacle.transform.position);
 
             keyObstacle.GetComponentInChildren<SpriteRenderer>().color = Color.red;
         }
