2fbf4729c66a1bb33924d39c91b26485f6c29a52
Laura Huovinen <laura.huovinen@outlook.com>
2020-11-26 12:28:13 +0200

    Help screen
    - Add: HelpUI class
    - Player can open and close a help screen in the main menu

---
+++ Assets/Scripts/UI/HelpUI.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Bomberfox.UI
+{
+    public class HelpUI : MonoBehaviour
+    {
+        [SerializeField] private GameObject help;
+
+        void Start()
+        {
+
+        }
+
+        public void ActivateMenu()
+        {
+            help.SetActive(true);
+        }
+
+        public void DeactivateMenu()
+        {
+            help.SetActive(false);
+        }
+    }
+}
