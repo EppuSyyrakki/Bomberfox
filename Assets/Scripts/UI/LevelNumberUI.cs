154fa32bc2cd8053ed3b295e87e3141772fb1e5f
Laura Huovinen <laura.huovinen@outlook.com>
2020-11-30 14:01:21 +0200

    Level number works
    - Correct level number shows at the beginning of the level
    - Added: LevelNumberUI class

---
+++ Assets/Scripts/UI/LevelNumberUI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Bomberfox.UI
+{
+    public class LevelNumberUI : MonoBehaviour
+    {
+        [SerializeField] public TextMeshProUGUI levelNumber;
+
+        public void Start()
+        {
+            levelNumber.text = "Level " + (GameManager.Instance.CurrentLevel).ToString();
+        }
+
+        void Update()
+        {
+
+        }
+    }
+
+}
